fbb7480bd622f2d2b8a7111168350c95cb8af45e
Eddges <shivamshekhar.i7@gmail.com>
2021-03-11 21:29:45 +0530

    Message emit to all users done, sending on Enter key added

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -13,3 +13,24 @@
 // socket.on("chat message", (msg) => {
 //     console.log("Message: ", msg);
 // });
+
+const socket = io()
+
+const chatInput = document.querySelector('#chatInput')
+const chatSend = document.querySelector('#chatSend')
+
+chatInput.addEventListener('keyup', e => {
+    if (e.keyCode === 13) {
+        chatSend.click()
+    }
+})
+
+chatSend.addEventListener('click', () => {
+    if (chatInput.value) {
+        socket.emit('sendMessage', chatInput.value)
+    }
+})
+
+socket.on('sendMessage', msg => {
+    console.log('Message: ', msg)
+})
--- socket.js
+++ socket.js
@@ -3,9 +3,9 @@ const io = require('socket.io')(http)
 
 io.on('connection', socket => {
     console.log('User connected')
-    socket.on('chat message', msg => {
+    socket.on('sendMessage', msg => {
         console.log('Chat value: ', msg)
-        socket.broadcast.emit('chat message', msg)
+        socket.broadcast.emit('sendMessage', msg)
     })
     socket.on('disconnect', () => {
         console.log('User disconnected')
