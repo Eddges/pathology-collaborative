35e6c29c74d3f9713997da97440d7e0668e7422e
Eddges <shivamshekhar.i7@gmail.com>
2021-03-11 18:10:26 +0530

    Main project with proper UI initialised

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname+'/public'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
---
+++ public/scripts/main.js
@@ -0,0 +1,15 @@
+// const socket = io();
+// var form = document.getElementById("form");
+// var input = document.getElementById("input");
+
+// form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     if (input.value) {
+//         socket.emit("chat message", input.value);
+//         input.value = "";
+//     }
+// });
+
+// socket.on("chat message", (msg) => {
+//     console.log("Message: ", msg);
+// });
