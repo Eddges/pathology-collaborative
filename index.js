9b39681d07344265b16846d72685efed94e87ce1
Eddges <shivamshekhar.i7@gmail.com>
2021-03-11 15:05:14 +0530

    Socket connection on http established

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const express = require('express')
-const http = require('http')
-// const io = require()
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+io.on('connection', socket => {
+    console.log('User connected')
+})
+
+http.listen(process.env.PORT || 3000, () => {
+    console.log('Server listening on port: ', process.env.PORT || 3000)
+})
