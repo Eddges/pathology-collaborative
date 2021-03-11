258f25a7f28da626517a0f669dad3c2f05737cc3
Eddges <shivamshekhar.i7@gmail.com>
2021-03-11 22:23:09 +0530

    Redirection to login page added if no user or room in localstorage

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,20 +1,24 @@
-const socket = io()
-
-const chatInput = document.querySelector('#chatInput')
-const chatSend = document.querySelector('#chatSend')
-
-chatInput.addEventListener('keyup', e => {
-    if (e.keyCode === 13) {
-        chatSend.click()
-    }
-})
-
-chatSend.addEventListener('click', () => {
-    if (chatInput.value) {
-        socket.emit('sendMessage', chatInput.value)
-    }
-})
-
-socket.on('sendMessage', msg => {
-    console.log('Message: ', msg)
-})
+if (!(localStorage.getItem('user') && localStorage.getItem('room'))) {
+    window.location.href = '/login'
+} else {
+    const socket = io()
+    
+    const chatInput = document.querySelector('#chatInput')
+    const chatSend = document.querySelector('#chatSend')
+    
+    chatInput.addEventListener('keyup', e => {
+        if (e.keyCode === 13) {
+            chatSend.click()
+        }
+    })
+    
+    chatSend.addEventListener('click', () => {
+        if (chatInput.value) {
+            socket.emit('sendMessage', chatInput.value)
+        }
+    })
+    
+    socket.on('sendMessage', msg => {
+        console.log('Message: ', msg)
+    })
+}
