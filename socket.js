baa13c2270326f5cf163c221fa03e805dfebb7ed
Eddges <shivamshekhar.i7@gmail.com>
2021-03-11 15:38:58 +0530

    Socket broadcasting done

--- socket.js
+++ socket.js
@@ -3,8 +3,9 @@ const io = require('socket.io')(http)
 
 io.on('connection', socket => {
     console.log('User connected')
-    socket.on('chat message', value => {
-        console.log('Chat value: ', value)
+    socket.on('chat message', msg => {
+        console.log('Chat value: ', msg)
+        socket.broadcast.emit('chat message', msg)
     })
     socket.on('disconnect', () => {
         console.log('User disconnected')
