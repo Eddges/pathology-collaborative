3b904b5ff7053683087e04e24fced5fa95731f3e
Eddges <shivamshekhar.i7@gmail.com>
2021-03-13 17:17:49 +0530

    OpenSeaDragon script, high resolution XML added, viewer.js created and slide displayed

---
+++ public/scripts/viewer.js
@@ -0,0 +1,6 @@
+const viewer = OpenSeadragon({
+    id: "openSeaDragonContainer",
+    prefixUrl: "assets/openSeaDragon/images/",
+    tileSources: 'assets/highRes/tissue_high.xml',
+    debugMode: false
+});
