79437618382c9fe11c4b8c21df57bc86227d25a2
Eddges <shivamshekhar.i7@gmail.com>
2021-03-13 18:19:39 +0530

    Sync and unsync on clicking the pills done

--- public/scripts/viewer.js
+++ public/scripts/viewer.js
@@ -1,5 +1,20 @@
 let syncViewer = false
 
+const syncButton = document.querySelector('.SyncPill')
+const unsyncButton = document.querySelector('.UnSyncPill')
+
+syncButton.addEventListener('click', () => {
+    syncViewer = true
+    syncButton.style.display = 'none'
+    unsyncButton.style.display = 'flex'
+})
+
+unsyncButton.addEventListener('click', () => {
+    syncViewer = false
+    syncButton.style.display = 'flex'
+    unsyncButton.style.display = 'none'
+})
+
 const viewer = OpenSeadragon({
     id: "openSeaDragonContainer",
     prefixUrl: "assets/openSeaDragon/images/",
@@ -13,7 +28,9 @@ viewer.addHandler('animation-finish', data => {
     console.log('zoom: ', zoom)
     console.log('data: ', data)
     console.log('center: ', center)
-    socket.emit('viewerEvent', { zoom, center })
+    if(syncViewer) {
+        socket.emit('viewerEvent', { zoom, center })
+    }
 })
 
 socket.on('viewerEvent', ({ zoom, center }) => {
